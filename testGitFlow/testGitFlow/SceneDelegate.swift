d3726fc9f2b5984f6300886ed5b58a233680942d
Евгений Зверик <death6543211@gmail.com>
2021-03-08 16:13:01 +0300

    добавил в принт

--- testGitFlow/testGitFlow/SceneDelegate.swift
+++ testGitFlow/testGitFlow/SceneDelegate.swift
@@ -13,7 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        print("scene")
+        print("цц scene")
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
